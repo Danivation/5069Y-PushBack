890d47e289fd67653a0570880f53ad2f24c7dc36
Daniel <divelasco7@gmail.com>
2025-06-03 17:52:39 -0500

    odometry code

--- include/odom.hpp
+++ include/odom.hpp
@@ -9,6 +9,7 @@ public:
     public:
         tracker(vex::rotation *sensor, float offset, float wheelSize);
         float getPosition();
+        float getOffset();
 
     private:
         vex::rotation *p_sensor;
@@ -31,40 +32,3 @@ extern odometry Odometry;
 
 
 
-
-
-class odom {
-private:
-    bool odomEnabled = true;
-
-    float getVerticalChange(float& previousVerticalPosition);
-    float getHorizontalChange(float& previousHorizontalPosition);
-    float getChangeInRotation(float& previousRotation);
-    void runOdom(float initialX, float initialY, float initialHeading);
-
-    float ForwardTracker_center_distance = 0;
-    float SidewaysTracker_center_distance = 0;
-    float ForwardTracker_position = 0;
-    float SideWaysTracker_position = 0;
-
-    float prevForward = 0;
-    float prevSideways = 0;
-    float prevTheta = 0;
-
-public:
-    // Current pose used elsewhere
-    VecPose currentOdomPose = {0, 0, 0};
-
-
-    float X_position = 0;
-    float Y_position = 0;
-    odom();
-
-    void startOdom(float startX, float startY, float startHeading);
-    void setPose(float x, float y, float heading);
-    //VecPose getPose();
-    //void follow(CubicBezier curve, bool forwards, float timeout, bool splitCurve);
-    void update_position(float ForwardTracker_position, float SidewaysTracker_position, float orientation_deg);
-
-};
-extern odom Odom;
